3b17e61ad75b3176c52f838dce6aeb6e144c9245
Zunaid Ansari <mohdzunaidalameenansari@gmail.com>
2025-03-01 19:38:49 +0530

    initial commit

---
+++ accounts/src/main/java/com/zunaidansari/accounts/AccountsApplication.java
@@ -0,0 +1,48 @@
+package com.zunaidansari.accounts;
+
+import com.zunaidansari.accounts.dto.AccountsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableFeignClients
+/*@ComponentScans({ @ComponentScan("com.zunaidansari.accounts.controller") })
+@EnableJpaRepositories("com.zunaidansari.accounts.repository")
+@EntityScan("com.zunaidansari.accounts.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "YourBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Zunaid Ansari",
+						email = "mohdzunaidalameenansari@gmail.com",
+						url = "https://www.zunaidansari.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.zunaidansari.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "YourBank Accounts microservice REST API Documentation",
+				url = "https://www.zunaidansari.com/swagger-ui.html"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
---
+++ accounts/src/main/java/com/zunaidansari/accounts/controller/CustomerController.java
@@ -0,0 +1,69 @@
+package com.zunaidansari.accounts.controller;
+
+import com.zunaidansari.accounts.dto.CustomerDetailsDto;
+import com.zunaidansari.accounts.dto.ErrorResponseDto;
+import com.zunaidansari.accounts.service.ICustomersService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Pattern;
+import org.apache.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(
+        name = "REST API for Customers in YourBank",
+        description = "REST APIs in YourBank to FETCH customer details"
+)
+@RestController
+@RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@Validated
+public class CustomerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
+    private final ICustomersService iCustomersService;
+
+    public CustomerController(ICustomersService iCustomersService){
+        this.iCustomersService = iCustomersService;
+    }
+
+    @Operation(
+            summary = "Fetch Customer Details REST API",
+            description = "REST API to fetch Customer details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/fetchCustomerDetails")
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestHeader("yourbank-correlation-id")
+                                                                       String correlationId,
+                                                                    @RequestParam @Pattern(regexp="(^$|[0-9]{10})",
+                                                                            message = "Mobile number must be 10 digits")
+                                                                   String mobileNumber) {
+        logger.debug("yourBank-correlation-id found: {} ", correlationId);
+        CustomerDetailsDto customerDetailsDto = iCustomersService.fetchCustomerDetails(mobileNumber, correlationId);
+        return ResponseEntity.status(HttpStatus.SC_OK).body(customerDetailsDto);
+
+    }
+
+
+}
